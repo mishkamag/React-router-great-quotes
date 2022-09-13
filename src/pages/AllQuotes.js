f43ebde41f4016329d4d391da52407dd0f9b2c41
mishkamag <m93.maglaperidze@gmail.com>
2022-09-13 22:22:10 +0400

    working with dumyy data

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,12 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "avto", text: "sichqare kaia cekvashi" },
+  { id: "q2", author: "zaza", text: "ras shvebi sait xar" },
+];
+
 const AllQoutes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQoutes;
