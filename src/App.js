790d6f95af7209569941fc653b150ace535ee5c5
mishkamag <m93.maglaperidze@gmail.com>
2022-09-13 21:54:18 +0400

    main nav and wrapper

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import { Route, Switch, Redirect } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 import AllQoutes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
 import QuoTeDetail from "./pages/QuoteDetail";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Redirect to="/quotes" />
-      </Route>
-      <Route path="/quotes" exact>
-        <AllQoutes />
-      </Route>
-      <Route path="/quotes/:qouteId">
-        <QuoTeDetail />
-      </Route>
-      <Route path="/new-quotes">
-        <NewQuote />
-      </Route>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQoutes />
+        </Route>
+        <Route path="/quotes/:qouteId">
+          <QuoTeDetail />
+        </Route>
+        <Route path="/new-quotes">
+          <NewQuote />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
