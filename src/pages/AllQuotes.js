8e08523af19825d98c311af42485b2ade176f40c
mishkamag <m93.maglaperidze@gmail.com>
2022-09-14 22:46:18 +0400

    Sending & Getting Quote Data via Http

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,12 +1,40 @@
+import { useEffect } from "react";
+
 import QuoteList from "../components/quotes/QuoteList";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import NoQuotesFound from "../components/quotes/NoQuotesFound";
+import useHttp from "../hooks/use-http";
+import { getAllQuotes } from "../lib/api";
 
-const DUMMY_QUOTES = [
-  { id: "q1", author: "avto", text: "sichqare kaia cekvashi" },
-  { id: "q2", author: "zaza", text: "ras shvebi sait xar" },
-];
+const AllQuotes = () => {
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
 
-const AllQoutes = () => {
-  return <QuoteList quotes={DUMMY_QUOTES} />;
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered focused">{error}</p>;
+  }
+
+  if (status === "completed" && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />;
+  }
+
+  return <QuoteList quotes={loadedQuotes} />;
 };
 
-export default AllQoutes;
+export default AllQuotes;
